84b8d01590c01dfb3522c52df1477d99dacc85ef
Aman Agrawal <582002aman@gmail.com>
2024-08-11 12:12:40 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { useCallback, useEffect, useState } from 'react'
+import Button from '../components/Button'
+import ClickCount from '../components/ClickCount'
+import styles from '../styles/home.module.css'
+
+function Home() {
+
+  return (
+    <div>Aman</div>
+  )
+}
+
+export default Home
