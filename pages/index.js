7b41291b3874347464acca63aba4d26bbcc72fae
Aman Agrawal <582002aman@gmail.com>
2024-08-14 10:15:35 +0000

    added server + api route

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,21 @@ import { useCallback, useEffect, useState } from 'react'
 import Button from '../components/Button'
 import ClickCount from '../components/ClickCount'
 import styles from '../styles/home.module.css'
+import Link from 'next/link'
 
 function Home() {
+  const [shortenedUrl, setShortenedUrl] = useState(null)
 
   return (
-    <div>Aman</div>
+    <div>
+      <h3>Enter a URL to be shorten</h3>
+      <input type="text" id="url" name="url" />
+      <Button>Shorten</Button>
+      
+      <div className={styles.shortenedUrl}>
+        {shortenedUrl && (<Link href={shortenedUrl}>{shortenedUrl}</Link>)}
+      </div>
+    </div>
   )
 }
 
---
+++ server.js
@@ -0,0 +1,47 @@
+// server.js
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // Route for the homepage
+  server.get('/', (req, res) => {
+    return app.render(req, res, '/');
+  });
+
+  server.get('/404', (req, res) => {
+    return app.render(req, res, '/404');
+  });
+
+  // Route for short URLs
+  server.get('/:shortURL', (req, res) => {
+    const { shortURL } = req.params;
+
+    const urlMapping = {
+      example: 'https://www.example.com',
+    };
+
+    const originalURL = urlMapping[shortURL];
+
+    if (originalURL) {
+      res.redirect(301, originalURL);
+    } else {
+      app.render(req, res, '/404');
+    }
+  });
+
+  // Fallback handler for all other routes
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
